5edb8e2e1bd479d123e629c544cb1b37149af088
houzhi <xxxzhi@gmail.com>
2015-05-24 17:40:27 +0800

    reinit

--- classifer/decision_tree.py
+++ classifer/decision_tree.py
@@ -1,5 +1,24 @@
+from sklearn import tree
 from base import BaseClassifier
 
 __author__ = 'houzhi'
 
 class DecisionTreeClassifier(BaseClassifier):
+    """
+    depending the sklearn DecisionTreeClassifier
+    """
+    def __init__(self):
+        super(DecisionTreeClassifier, self).__init__()
+        self.clf = tree.DecisionTreeClassifier()
+
+    def train(self, x, y):
+        self.clf.fit(x, y)
+
+
+    def predict(self, x):
+        """
+        predict
+        :param x:
+        :return:
+        """
+        return self.clf.predict(x)
