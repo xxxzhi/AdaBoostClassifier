e5161cad03bdeb1c353b1c06dc32752a34c160d3
houzhi <xxxzhi@gmail.com>
2015-05-27 16:01:10 +0800

    add process_alpha method

--- adaboost.py
+++ adaboost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import dbm
 from sklearn.datasets import load_iris
 
 from classifer.base import BaseClassifier
@@ -22,6 +23,17 @@ class AbsAdaBoostClassifier(BaseClassifier):
         :return: BaseClassifier
         """
         pass
+
+    def process_alpha(self, index, alpha):
+        """
+        after a successful classifier training. this method will be called
+
+        :param index
+        :param alpha
+        :return:
+        """
+        print '------------alpha-----------'
+        print alpha
 
     def train(self, x, y):
         """
@@ -61,10 +73,12 @@ class AbsAdaBoostClassifier(BaseClassifier):
                 alpha = 4.0
             elif error > 0.7:
                 print 'Discarding learner'
+                print n
                 continue  # discard classifier with error > 0.5
             else:
                 alpha = 0.5 * np.log((1 - error) / error)
 
+            self.process_alpha(n, alpha)
             alphas.append(alpha)
             classifiers.append(weak_classifier)
             print 'weak learner added'
@@ -93,35 +107,41 @@ class AbsAdaBoostClassifier(BaseClassifier):
         @:return labels
         """
 
-        labels = []
-        for row in x:
-            res = AbsAdaBoostClassifier.classify(self.clf, row)
-            labels.append(res)
-        print x.shape
-        return np.asarray(labels)
+        result_list = []
+        weight_list = []
+        for (weight, classifier) in self.clf:
+            res = classifier.predict(x)
+            result_list.append(res)
+            weight_list.append(weight)
 
-    @staticmethod
-    def classify(weight_classifier, example):
-        classification = 0
-        result_map = {}
-        for (weight, classifier) in weight_classifier:
-            res = classifier.predict(example)
-            if str(classifier.predict(example)) == str(1):
-                ex_class = 1
-            else:
-                ex_class = -1
-            classification += weight * ex_class
-            if not result_map.has_key(str(res[0])):
-                result_map[str(res[0])] = 0
-            result_map[str(res[0])] = result_map[str(res[0])] + weight
+        res =[]
+        for i in range(len(result_list[0])):
+            result_map = {}
+            for j in range(len(result_list)):
+                if not result_map.has_key(str(result_list[j][i])):
+                    result_map[str(result_list[j][i])] = 0
+                result_map[str(result_list[j][i])] = result_map[str(result_list[j][i])] + weight_list[j]
 
-        cur_max_value = -10000000000000000000000
-        max_key = ''
-        for key in result_map:
-            if result_map[key] > cur_max_value:
-                cur_max_value = result_map[key]
-                max_key = key
-        return int(max_key)
+            cur_max_value = -10000000000000000000000
+            max_key = ''
+            for key in result_map:
+                if result_map[key] > cur_max_value:
+                    cur_max_value = result_map[key]
+                    max_key = key
+
+            res.append(int(max_key))
+        return np.asarray(res)
+
+    def load(self, weight=[]):
+        """
+        reload model base on weight of each classifier
+        :param weight:
+        :return:
+        """
+        classifiers = []
+        for index in range(len(weight)):
+            classifiers.append(self.create_classifer(index))
+        self.clf = zip(weight, classifiers)
 
     @staticmethod
     def resample(weights):
@@ -144,7 +164,12 @@ class DecisionAdaBoostClassifier(AbsAdaBoostClassifier):
     def create_classifer(self, index=0):
         return DecisionTreeClassifier()
 
-
+    def process_alpha(self, index, alpha):
+        super(DecisionAdaBoostClassifier, self).process_alpha(index, alpha)
+        db = dbm.open('params.pag','c')
+        key = 'alpha_'+str(index)
+        db[key] = str(alpha)
+        db.close()
 
 def test():
     n_classes = 3
@@ -154,64 +179,66 @@ def test():
     iris = load_iris()
     import matplotlib.pyplot as plt
 
-    for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
-                                    [1, 2], [1, 3], [2, 3]]):
-        # We only take the two corresponding features
-        X = iris.data[:, pair]
-        y = iris.target
+    # We only take the two corresponding features
+    pairidx = 0
+    pair =[0,1]
+    X = iris.data[:, pair]
+    y = iris.target
 
-        # Shuffle
-        idx = np.arange(X.shape[0])
-        np.random.seed(13)
-        np.random.shuffle(idx)
-        X = X[idx]
-        y = y[idx]
+    # Shuffle
+    idx = np.arange(X.shape[0])
+    np.random.seed(13)
+    np.random.shuffle(idx)
+    X = X[idx]
+    y = y[idx]
 
-        # Standardize
-        mean = X.mean(axis=0)
-        std = X.std(axis=0)
-        X = (X - mean) / std
+    # Standardize
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    X = (X - mean) / std
 
-        # Train
-        clf = DecisionAdaBoostClassifier(num_rounds=3)
-        # clf = DecisionTreeClassifier()
-        # print X
-        print y
-        clf.train(X, y)
+    # Train
+    clf = DecisionAdaBoostClassifier(num_rounds=3)
+    # clf = DecisionTreeClassifier()
+    # print X
+    print y
+    clf.train(X, y)
 
-        # Plot the decision boundary
-        plt.subplot(2, 3, pairidx + 1)
+    # Plot the decision boundary
+    plt.subplot(2, 3, pairidx + 1)
 
-        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
-                             np.arange(y_min, y_max, plot_step))
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
 
-        print '----'
-        print iris.data[:1,  ]
-        values = np.c_[xx.ravel(), yy.ravel()]
-        Z = clf.predict(values)
-        print Z
-        print Z.shape
-        print xx.shape
-        Z = Z.reshape(xx.shape)
-        cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+    print '----'
+    print iris.data[:1,  ]
+    values = np.c_[xx.ravel(), yy.ravel()]
+    Z = clf.predict(values)
+    print Z
+    print Z.shape
+    print xx.shape
+    Z = Z.reshape(xx.shape)
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
 
-        plt.xlabel(iris.feature_names[pair[0]])
-        plt.ylabel(iris.feature_names[pair[1]])
-        plt.axis("tight")
+    plt.xlabel(iris.feature_names[pair[0]])
+    plt.ylabel(iris.feature_names[pair[1]])
+    plt.axis("tight")
 
-        # Plot the training points
-        for i, color in zip(range(n_classes), plot_colors):
-            idx = np.where(y == i)
-            plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
-                        cmap=plt.cm.Paired)
+    # Plot the training points
+    for i, color in zip(range(n_classes), plot_colors):
+        idx = np.where(y == i)
+        plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
+                    cmap=plt.cm.Paired)
 
-        plt.axis("tight")
+    plt.axis("tight")
 
     plt.suptitle("Decision surface of a decision tree using paired features")
     plt.legend()
     plt.show()
+
+
 
 
 if __name__ == '__main__':
